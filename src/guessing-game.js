78240d963784740fd27ede3b4035ecad118ccdf5
doritozbae <doritosbae@mail.ru>
2021-09-03 17:26:23 +0300

    feat:  add func

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,30 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    this.min = 0; 
+    this.max = 0;
+    this.mid = 0;
+    }
 
     setRange(min, max) {
-
+    this.min = min; 
+    this.max = max;
     }
 
     guess() {
-
+     this.mid = Math.ceil((this.min + this.max) / 2);
+     return (this.mid); 
     }
 
     lower() {
+        this.setRange(this.min , this.mid);
 
     }
 
     greater() {
-
+        this.setRange(this.mid , this.max);
     }
+
 }
 
 module.exports = GuessingGame;
+
